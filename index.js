28fd58a80061d69f786202ace1208aea43021f8c
Mark Lubkowitz <mail@mark-lubkowitz.de>
2023-05-15 08:39:03 +0200

    Some code optimizations

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 import __wordslist from './wordslist.js'
 
 class taggert {
+    seperator = '-'
+
     constructor (wordslist = null) {
         this.wordslist = wordslist || __wordslist
     }
 
+    /*
+    Creates a gamer tag based on all imported wordslists following the order of the lists in the object.
+    */
     new () {
         return Object
             .getOwnPropertyNames(this.wordslist)
             .map(
-                k => this
-                    .wordslist[k][
-                        Math.floor((Math.random() * this.wordslist[k].length))
-                    ]
+                k => this._random(this.wordslist[k])
             )
-            .join('-')
+            .join(this.seperator)
     }
 
+    /*
+    Creates a gamer tag based on a given pattern.
+    Provide an array of wordslists names to define the patter, i.e. ['adjective','color','fruit-vegetable']
+    */
     make (pattern) {
         if (!Array.isArray(pattern))
             throw new Error('The pattern for make() must be of type array.')
@@ -26,12 +32,19 @@ class taggert {
                 k => this.wordslist.hasOwnProperty(k)
             )
             .map(
-                k => this
-                    .wordslist[k][
-                        Math.floor((Math.random() * this.wordslist[k].length))
-                    ]
+                k => this._random(this.wordslist[k])
             )
-            .join('-')
+            .join(this.seperator)
+    }
+
+    /*
+    Internal function to select a random element from an array.
+    */
+    _random (anArray) {
+        if (!Array.isArray(anArray))
+            return ''
+        else
+            return anArray[Math.floor((Math.random() * anArray.length))]
     }
 }
 
